ea49969379cda78a30d9887b2a3550b4ea1ff5b5
davizn-lab <dmfavatto@gmail.com>
2026-05-22 14:29:06 -0300

    Implement bike card rendering function
    Added a function to render bike cards dynamically.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ const titulo = document.getElementById("titulo");
 titulo.addEventListener("click", function() {
     titulo.style.color = "blue";
 });
+function renderizarCards(bike) {
+    const novoCard = `
+        <div class="card">
+            <div class="letras">
+                <h3 id="titulo">${bike.titulo}</h3>
+                <p>${bike.texto}</p>
+            </div>
+
+            <div class="img">
+                <img src="${bike.imagem}" alt="${bike.descricao}"/>
+            </div>
+        </div>
+    `;
+
+    colecao.innerHTML += novoCard;
+}
